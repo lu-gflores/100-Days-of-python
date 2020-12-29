3a821f743cbe733676edf590ef0b28b9fd2be721
George Flores <georgeflores886@gmail.com>
2020-12-29 10:42:17 -0500

    added band nam gen

---
+++ Day-1-Python-Variables/BandNameGen.py
@@ -0,0 +1,4 @@
+print('Welcome to the Band Name Generator!')
+city = input('What city did you grow up in?\n')
+pet = input("Enter your pet's name\n")
+print("Your band name can be " + city + " " + pet)
