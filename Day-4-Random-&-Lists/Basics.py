2bb9a1b192cc64f88827c20ce8ba831d26f78551
George Flores <georgeflores886@gmail.com>
2021-01-01 20:56:30 -0500

    added exercise

--- Day-4-Random-&-Lists/Basics.py
+++ Day-4-Random-&-Lists/Basics.py
@@ -42,3 +42,52 @@ print(states)
 # results in error since index is out of range
 print(states[23])
 
+# 4 - 1 Exercise
+import random
+
+coin = random.randint(0, 1)
+
+if coin == 1:
+  print(f'Heads')
+else:
+  print(f'Tails')
+
+# 4 -2 Exercise
+# # Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+# 🚨 Don't change the code above 👆
+import random
+#Write your code below this line 👇
+
+length_names = len(names)
+random_person = random.randint(0, length_names - 1)
+paying_person = names[random_person]
+
+print(f'{paying_person} will be covering the meal!')
+
+
+# 4- 3
+
+# 🚨 Don't change the code below 👇
+row1 = ["⬜️","⬜️","⬜️"]
+row2 = ["⬜️","⬜️","⬜️"]
+row3 = ["⬜️","⬜️","⬜️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+
+col = int(position[0]) - 1
+row = int(position[1]) - 1
+
+map[row][col] = 'X'
+
+
+#Write your code above this row 👆
+
+# 🚨 Don't change the code below 👇
+print(f"{row1}\n{row2}\n{row3}")
+
