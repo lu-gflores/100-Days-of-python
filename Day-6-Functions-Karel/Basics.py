36408a6d3558eda550f0f56150fb1edeac47a774
George Flores <georgeflores886@gmail.com>
2021-01-04 15:13:37 -0500

    completed exercies

--- Day-6-Functions-Karel/Basics.py
+++ Day-6-Functions-Karel/Basics.py
@@ -87,3 +87,27 @@ while not at_goal():
         jump()
     else:
         move()
+
+# 6 - 4 Advanced randomized hurdle jumpoing
+
+def turn_around():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def jump():
+    turn_left()
+    while wall_on_right():
+        move()
+    turn_around()
+    move()
+    turn_around()
+    while front_is_clear():
+        move()
+    turn_left()
+    
+while not at_goal():
+    if wall_in_front():
+        jump()
+    else:
+        move()
