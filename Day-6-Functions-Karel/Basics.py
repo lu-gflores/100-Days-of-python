4bb8c6f71bcd73a82f6f447eb08c094f551673d2
George Flores <georgeflores886@gmail.com>
2021-01-03 22:50:48 -0500

    added while loop

--- Day-6-Functions-Karel/Basics.py
+++ Day-6-Functions-Karel/Basics.py
@@ -11,6 +11,21 @@ def my_function():
   print('Goodbye')
 my_function()
 
+# while loop
+while something_is_true:
+    #Do this
+    #then this
+    #etc...
+
+number_of_hurdeles = 6
+
+#loops through number of hurdles while this condition is true
+while number_of_hurdeles > 0:
+    number_of_hurdeles =-1
+    print(number_of_hurdeles)
+
+
+
 ##Exercies are from the Reeborg's world site 
 # 6 - 1 Make a square
 def turn_around():
@@ -45,4 +60,8 @@ def jump():
     turn_left()
 
 for num in range(6):
+    jump()
+
+#will loop until jump is equal to at_goal()
+while not at_goal():
     jump()
